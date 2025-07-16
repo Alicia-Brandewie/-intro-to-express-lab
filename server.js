d5d04ca7f6216c8f12b12927ace9b35a2e2aadf3
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-15 23:28:48 -0500

    one through four complete

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/greetings/:username', (req, res) => {
 
 //2. Rolling the Dice
 
-app.get('/roll/:number', (req,res) => {
+app.get('/roll/:number', (req, res) => {
     const number = req.params.number;
     if (isNaN(number)) {
         res.send(`You must specify a number.`);
@@ -16,21 +16,21 @@ app.get('/roll/:number', (req,res) => {
         const roll = Math.floor(Math.random() * (req.params.number));
         res.send(`You rolled a ${roll}.`)
     }
- });
+});
 
 //3. I Want THAT One!
 
-   const collectibles = [
+const collectibles = [
     { name: 'shiny ball', price: 5.95 },
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-  ];
+];
 
 app.get('/collectibles/:index', (req, res) => {
     const index = req.params.index;
     const i = parseInt(index);
     if (!collectibles[i]) { // "if there is nothing there, return something at this index"
-    res.send(`This item is not yet in stock. Check back soon!`); 
+        res.send(`This item is not yet in stock. Check back soon!`);
     } else {
         const name = req.query.name;
         const price = req.query.price;
@@ -40,17 +40,17 @@ app.get('/collectibles/:index', (req, res) => {
 
 //4. Filter Shoes by Query Parameters
 
-  const shoes = [
-      { name: "Birkenstocks", price: 50, type: "sandal" },
-      { name: "Air Jordans", price: 500, type: "sneaker" },
-      { name: "Air Mahomeses", price: 501, type: "sneaker" },
-      { name: "Utility Boots", price: 20, type: "boot" },
-      { name: "Velcro Sandals", price: 15, type: "sandal" },
-      { name: "Jet Boots", price: 1000, type: "boot" },
-      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
-  ];
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
-app.get('/shoes', (req, res)=> {
+app.get('/shoes', (req, res) => {
     const shoesArray = req.params.shoes
     const i = parseInt(shoes);
 
@@ -59,15 +59,15 @@ app.get('/shoes', (req, res)=> {
     const type = req.query.type;
 
     if (minPrice || maxPrice || type) {
-        const filteredShoes = shoes.filter((shoe)=>{
+        const filteredShoes = shoes.filter((shoe) => {
             if (shoe.price <= maxPrice && shoe.price >= minPrice && shoe.type === type) {
                 return shoe
             }
-            res.send(filteredShoes)\
-});
-} else{
-    res.send(shoes) 
-}
+            res.send(filteredShoes)
+        });
+    } else {
+        res.send(shoes)
+    }
 });
 
 app.listen(3000, () => {
